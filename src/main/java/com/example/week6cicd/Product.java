1cea60ac129f277f52de7a0a1e5c92f8424ac855
G00422935@atu.ie <G00422935@atu.ie>
2024-10-23 15:18:29 +0100

    Added Product, ProductController and ProductService class.

---
+++ src/main/java/com/example/week6cicd/Product.java
@@ -0,0 +1,13 @@
+package com.example.week6cicd;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private  int id;
+    private String name;
+    private double price;
+}
---
+++ src/main/java/com/example/week6cicd/Week5IoCApplication.java
@@ -0,0 +1,13 @@
+package com.example.week6cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week5IoCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week5IoCApplication.class, args);
+    }
+
+}
